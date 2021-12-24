6c3ec38f10a58cfb9747fd07fc228f8c185b0c3a
Qxornet <44473060+qxornet@users.noreply.github.com>
2021-12-24 15:38:49 +0400

    (all): init commit
    loaded main files

---
+++ MathExpHandler.cpp
@@ -0,0 +1,118 @@
+﻿#include "mathexphandler.hpp"
+
+MathExpHandler::MathExpHandler()
+{
+
+}
+
+void MathExpHandler::setFormatString(QString formula)
+{
+    format = formula;
+}
+
+void MathExpHandler::processing()
+{
+    stringConversion();
+
+    for(auto item : operands)
+    {
+        std::cout << item.toStdString() + " ";
+    }
+
+    std::cout << std::endl;
+}
+
+
+
+double MathExpHandler::getFinally() const
+{
+    return finally;
+}
+
+
+
+void MathExpHandler::addOperation(QString _opt)
+{
+    if(!_opt.compare(")"))
+    {
+        std::reverse(operations.begin(), operations.end());
+
+        auto founded = std::find(operations.begin(), operations.end(), "(");
+        if(founded == operations.end()) throw std::string("Syntax error");
+
+        operands.insert(operands.end(), operations.begin(), founded);
+        operations.erase(operations.begin(), founded+1);
+
+        std::reverse(operations.begin(), operations.end());
+        return;
+    }
+
+    if(!_opt.compare("(")) { operations.push_back(_opt); return; }
+    if(!operations.empty() && weightOpt[_opt] <= weightOpt[operations.back()])
+    {
+
+        operands.push_back(operations.back());
+        operations.pop_back();
+        operations.push_back(_opt);
+        return;
+    }
+
+    operations.push_back(_opt);
+}
+
+void MathExpHandler::addOperator(QString _opr)
+{
+    if(!_opr.isEmpty())
+        operands.push_back(_opr);
+}
+
+void MathExpHandler::stringConversion()
+{
+    operations.clear();
+    operands.clear();
+
+    QString symbol;
+    QString value;
+    bool isWaitOpt = false;
+    for(auto& chr : format)
+    {
+        if(!isWaitOpt) symbol = QString(chr);
+        else symbol += QString(chr);
+
+        if(!symbol.compare("$")) isWaitOpt = true;
+        if(weightOpt.find(symbol) != weightOpt.end())
+        {
+            try
+            {
+                if(!value.isEmpty())
+                {
+                    addOperator(value);
+                    value.clear();
+                }
+
+                addOperation(symbol);
+                symbol.clear();
+
+                isWaitOpt = false;
+            }
+            catch(std::string e)
+            {
+                std::cout << e << std::endl;
+            }
+        }
+        else
+        {
+            if(!symbol.compare(" ") || isWaitOpt) continue;
+            value += symbol;
+        }
+    }
+
+    addOperator(value);
+    operands.insert(operands.end(), operations.begin(), operations.end());
+    operations.clear();
+}
+
+MathExpHandler::~MathExpHandler()
+{
+
+}
---
+++ MathExpHandler.hpp
@@ -0,0 +1,79 @@
+﻿
+/************************************************
+ *
+ *              CREATED BY QXORNET
+ *
+ ************************************************/
+
+#pragma once
+
+#include <tuple>
+#include <vector>
+#include <array>
+#include <utility>
+#include <memory>
+#include <algorithm>
+#include <string>
+#include <any>
+#include <variant>
+#include <map>
+#include <iostream>
+
+#include <QString>
+#include <QDebug>
+
+class MathExpHandler
+{
+
+public:
+
+    MathExpHandler();
+   ~MathExpHandler();
+
+// methods
+public:
+
+    void setFormatString(QString formula);
+    void processing();
+
+    double getFinally() const;
+
+// methods
+private:
+
+    void addOperation(QString _opt);
+    void addOperator(QString _opr);
+
+    void stringConversion();
+
+// variables
+protected:
+
+
+// variables
+private:
+
+    QString format;
+
+    std::vector<QString> operations;
+    std::vector<QString> operands;
+
+    double finally {0.0};
+
+    std::map<QString, int> weightOpt = {
+        {"(", 1},
+        {")", 1},
+        {"/", 8},
+        {"^", 8},
+        {"*", 8},
+        {"-", 6},
+        {"+", 6},
+        {"%", 5},
+        {"$sqrt", 8},
+        {"$mod",  8},
+        {"$sin",  8},
+        {"$cos",  8},
+    };
+
+};
+
